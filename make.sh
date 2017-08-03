e6b920d1c8852649b316f332b0c3a2edb1f1d8f9
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-08-03 22:16:14 +0100

    small change to script

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# remeber to have 2 folders (src & build)
+# place a main lua file (main.lua) inside src
+# and set this in bashrc or zshrc
+# alias love="/Applications/love.app/Contents/MacOS/love"
+
 cd src
 zip -9 -r ../build/Game.love .
 /Applications/love.app/Contents/MacOS/love ../build/Game.love
